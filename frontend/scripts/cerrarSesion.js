e065972ad4b19d67619c322c58adbe4bf761e6e4
JohnPV894 <arenalessantiago1@gmail.com>
2025-05-28 17:02:38 +0200

    funcionalidades agregadas y corregirda de modal en crear Especialista

---
+++ frontend/scripts/cerrarSesion.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+      $("#cerrarSesion").click(function (e) { 
+            e.preventDefault();
+            window.location.href = "login.html"
+      });
+});
---
+++ frontend/scripts/especialistas.js
@@ -0,0 +1,100 @@
+"use strict";
+
+$(document).ready(function(){
+      let coleccionEspecialistas;
+      //Traer coleccion y hacer tarjetas
+      $.ajax({
+            type: "get",
+            url: "http://localhost:5000/especialistas",
+            data: "data",
+            dataType: "json",
+            contentType: "application/json",
+            success: async function ( response) {
+                  response.forEach(element => {
+                  $(".contenedor-cuadros").append(
+                        `<div class="tarjeta doctor">
+                          <div class="info-doctor">
+                            <img src="../img/perfilIcon.png" alt="Foto" class="avatar">
+                            <div class="texto">
+                              <strong>${element.nombre}</strong><br>
+                              Apellido ${element.apellido}<br><br>
+                              Rama: ${element.rama}<br>
+                              Nacimiento: ${element.fechaNacimiento}<br>
+                              DNI: ${element.dni}
+                            </div>
+                          </div>
+                        </div>`
+                  );
+                  });
+            },
+            error: function(xhr, status, error) {
+                  alert("Fallo al recuperar Tarjetas");
+                  alert("vuelva a intentarlo en unos minutos")
+                  console.error(error);
+            }
+            
+      });
+      
+
+      //Enviar formulario Crear paciente
+      $("#enviarCrearPaciente").click(function (e) { 
+            let nombre = $("#nombre").val();
+            let apellido = $("#apellido").val();
+            let dni = $("#dni").val();
+            let telefono = $("#telefono").val();
+            let fechaNacimiento = $("#fechaNacimiento").val();
+            let datos = {
+                        "dni":dni,
+                        "nombre":nombre,
+                        "apellido":apellido,
+                        "telefono":telefono,
+                        "fechaNacimiento":fechaNacimiento
+                  };
+            
+            $.ajax({
+                  type: "post",
+                  url: "http://localhost:5000/pacientes/crear",
+                  data: JSON.stringify(datos),
+                  dataType: "json",
+                  contentType: "application/json",
+                  success: function (response) {
+                        console.log(response);
+                        
+                  },
+                  error:alert("error al crear Paciente")
+            });
+            console.log("Crear paciente");
+            
+            
+      });
+
+      $("#desplegarCrearPaciente").click(function (e) { 
+            e.preventDefault();
+            $("#contenedorFormPaciente").fadeIn();
+            console.log("mostrar");
+            console.log(coleccionCitas);
+            
+      });
+      $("#cancelarCrearPaciente").click(function (e) { 
+            e.preventDefault();
+            $("#contenedorFormPaciente").fadeOut();
+            console.log("ocultar");
+      });
+
+      $("#desplegarCrearCita").click(function (e) { 
+            e.preventDefault();
+            $("#contenedorFormPaciente").fadeIn();
+            console.log("mostrar");
+            
+      });
+      $("#cancelarCrearCita").click(function (e) { 
+            e.preventDefault();
+            $("#contenedorFormPaciente").fadeOut();
+            console.log("ocultar");
+      });
+      
+
+
+
+
+})
