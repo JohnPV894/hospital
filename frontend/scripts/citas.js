6bd9b41af7eaefbb56bb657b9a6513f1b85b3617
John Santiago Arenales Fernandez <johnsaf@aulaestudio.es>
2025-06-02 20:01:34 +0200

    Generacion de tarjetas correcta dinamica y ademas bien estructurada

--- frontend/scripts/citas.js
+++ frontend/scripts/citas.js
@@ -1,25 +1,75 @@
 "use strict";
 
-$(document).ready(function(){
-      let coleccionCitas;
-      //Traer coleccion y hacer tarjetas
-      $.ajax({
-            type: "get",
-            url: "http://localhost:5000/citas",
-            data: "data",
-            dataType: "json",
-            contentType: "application/json",
-            success: async function ( response) {
-                  response.forEach(element => {
+$(document).ready(async function(){
+      async function recuperarColecciones (){
+            let citas;
+            let pacientes;
+            let especialistas;
+            await $.ajax({
+                  type: "get",
+                  url: "http://localhost:5000/pacientes",
+                  data: "data",
+                  dataType: "json",
+                  contentType: "application/json",
+                  success: function (response) {
+                        pacientes = response;
+                  }
+            });
+            await $.ajax({
+                  type: "get",
+                  url: "http://localhost:5000/especialistas",
+                  data: "data",
+                  dataType: "json",
+                  contentType: "application/json",
+                  success: function (response) {
+                         especialistas = response;
+                  }
+            });
+            await $.ajax({
+                  type: "get",
+                  url: "http://localhost:5000/citas",
+                  data: "data",
+                  dataType: "json",
+                  contentType: "application/json",
+                  success: function (response) {
+                         citas = response;
+                  }
+            });
+            return {"citas": citas,"especialistas":especialistas,"pacientes":pacientes}
+
+      }
+      async function generarTarjetas (){
+            let colecciones = await recuperarColecciones();
+            colecciones.citas.forEach(citaObj => {
+                  
+                        
+                  let nombrePaciente;
+                  let nombreEspecialista;
+                  let fechaFormateada=citaObj.fecha; // Tue, 12 May 2020 23:50:21 GMT;
+                  //Con la id recuperamos los datos de paciente      
+                  for (let index = 0; index < colecciones.pacientes.length; index++) {
+      
+                        if (citaObj.pacienteID===colecciones.pacientes[index]._id) {
+                              nombrePaciente=colecciones.pacientes[index].nombre+colecciones.pacientes[index].apellido;
+                        }
+                  }
+                  //con la id recuperamos los datos de especialista
+                  for (let index = 0; index < colecciones.especialistas.length; index++) {
+      
+                        if (citaObj.especialistaID===colecciones.especialistas[index]._id) {
+                              nombreEspecialista=colecciones.especialistas[index].nombre+colecciones.especialistas[index].apellido;
+                        }
+                  }
+                  console.log(nombrePaciente,nombrePaciente,fechaFormateada);
+                  
                   $(".contenedor-cuadros").append(
-                        `<div class="tarjeta" data-id=${element._id}>
+                        `<div class="tarjeta" data-id=${citaObj._id}>
                           <div class="info-doctor">
                             <img src="../img/perfilIcon.png" alt="Foto" class="avatar">
                             <div class="texto">
-                              <strong>${element.nombre}</strong><br>
-                              Apellido ${element.apellido}<br><br>
-                              Nacimiento: ${element.fechaNacimiento}<br>
-                              DNI: ${element.dni}
+                              Paciente: ${nombrePaciente} <br>
+                              Especialista: ${nombreEspecialista} <br>
+                              Fecha: ${fechaFormateada}
                               <span> 
                                     <input  type="button" value="Borrar" id="borrar"/>
                                     <input  type="button" value="Editar" id="editar"/>  
@@ -27,92 +77,55 @@ $(document).ready(function(){
                             </div>
                           </div>
                         </div>`
-                  );
-                  });
-            },
-            error: function(xhr, status, error) {
-                  alert("Fallo al recuperar Tarjetas");
-                  alert("vuelva a intentarlo en unos minutos")
-                  console.error(error);
-            }
-            
-      });
-
-
-      //Enviar formulario Crear paciente
-      $("#enviarCrearPaciente").click(function (e) { 
-            let nombre = $("#nombre").val();
-            let apellido = $("#apellido").val();
-            let dni = $("#dni").val();
-            let telefono = $("#telefono").val();
-            let fechaNacimiento = $("#fechaNacimiento").val();
-            let datos = {
-                        "dni":dni,
-                        "nombre":nombre,
-                        "apellido":apellido,
-                        "telefono":telefono,
-                        "fechaNacimiento":fechaNacimiento
-                  };
-            
-            $.ajax({
-                  type: "post",
-                  url: "http://localhost:5000/pacientes/crear",
-                  data: JSON.stringify(datos),
-                  dataType: "json",
-                  contentType: "application/json",
-                  success: function (response) {
-                        console.log(response);
-                        
-                  },
-                  error:alert("error al crear Paciente")
+                  )
+      
             });
-            console.log("Crear paciente");
-            
-            
-      });
+      }
 
-      $("#desplegarCrearPaciente").click(function (e) { 
-            e.preventDefault();
-            $("#contenedorFormPaciente").fadeIn();
-            console.log("mostrar");
-            console.log(coleccionCitas);
-            
-      });
-      $("#cancelarCrearPaciente").click(function (e) { 
-            e.preventDefault();
-            $("#contenedorFormPaciente").fadeOut();
-            console.log("ocultar");
-      });
+      await generarTarjetas();
 
-      $("#desplegarCrearCita").click(function (e) { 
-            e.preventDefault();
-            $("#contenedorFormPaciente").fadeIn();
-            console.log("mostrar");
-            
-      });
-      $("#cancelarCrearCita").click(function (e) { 
-            e.preventDefault();
-            $("#contenedorFormPaciente").fadeOut();
-            console.log("ocultar");
-      });
-      $(".contenedor-cuadros").on("click","#borrar", function (e) {
-            e.preventDefault();
-            let tarjeta = $(this).closest(".tarjeta");
-            let id = tarjeta.data("id")
-            console.log(id);
-            
-            $.ajax({
-                  type: "delete",
-                  url: "http://localhost:5000/citas/eliminar",
-                  data: JSON.stringify({id}),
-                  dataType: "json",
-                  contentType: "application/json",
-                  success: function (response) {
-                        if(response!=null){
-                              alert(response.mensaje);
-                        }
-                  }
-            });
-      });
+      //$("#desplegarCrearPaciente").click(function (e) { 
+      //      e.preventDefault();
+      //      $("#contenedorFormPaciente").fadeIn();
+      //      console.log("mostrar");
+      //      console.log(coleccionCitas);
+      //      
+      //});
+      //$("#cancelarCrearPaciente").click(function (e) { 
+      //      e.preventDefault();
+      //      $("#contenedorFormPaciente").fadeOut();
+      //      console.log("ocultar");
+      //});
+//
+      //$("#desplegarCrearCita").click(function (e) { 
+      //      e.preventDefault();
+      //      $("#contenedorFormPaciente").fadeIn();
+      //      console.log("mostrar");
+      //      
+      //});
+      //$("#cancelarCrearCita").click(function (e) { 
+      //      e.preventDefault();
+      //      $("#contenedorFormPaciente").fadeOut();
+      //      console.log("ocultar");
+      //});
+      //$(".contenedor-cuadros").on("click","#borrar", function (e) {
+      //      e.preventDefault();
+      //      let tarjeta = $(this).closest(".tarjeta");
+      //      let id = tarjeta.data("id")
+      //      console.log(id);
+      //      
+      //      $.ajax({
+      //            type: "delete",
+      //            url: "http://localhost:5000/citas/eliminar",
+      //            data: JSON.stringify({id}),
+      //            dataType: "json",
+      //            contentType: "application/json",
+      //            success: function (response) {
+      //                  if(response!=null){
+      //                        alert(response.mensaje);
+      //                  }
+      //            }
+      //      });
+      //});
       
 })
